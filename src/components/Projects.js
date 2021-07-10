dfc8f011abc097162199e650f63cbc5cc1fe36ab
HichemHa <hichemhanafi012@gmail.com>
2021-07-10 11:53:37 +0100

    responsive 3

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,10 +34,10 @@ function Projects() {
                 <li className="porject" style={{
                     backgroundImage: `url("${p3}")`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"
                 }}><a>E-silver Shop</a></li>
-                <li className="porject" style={{
+                <li className="porject" id="cp" style={{
                     backgroundImage: `url("https://scontent.ftun15-1.fna.fbcdn.net/v/t1.6435-9/100104261_279914393372364_3108041637662556160_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=973b4a&_nc_ohc=Pe1vpUUposcAX_2E_oV&_nc_ht=scontent.ftun15-1.fna&oh=04ba0580934826e5b9867a6c533de24e&oe=60E7EB17")`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"
                 }}><a>CapBon palette</a></li>
-                <li className="porject" style={{
+                <li className="porject" id="tt" style={{
                     backgroundImage: `url("https://tunisiatesol.org/wp-content/uploads/2021/04/TEsol.png")`, backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover"
                 }}><a href="http://tunisiatesol.org/ ">Tunisie Tesoal</a></li>
                 <li className="porject" style={{
